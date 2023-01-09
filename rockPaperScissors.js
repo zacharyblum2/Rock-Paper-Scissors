afe3858db3174d3e6ee2ac10a573a1e320c6bfcc
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-09 17:33:03 -0500

    Create function play that takes a user choice and computer choice and relays who won

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,3 +8,35 @@ function getComputerChoice() {
     // Return the randomly selected choice.
     return choices[choice];
 }
+
+function play(playerSelection, computerSelection) {
+    // Convert the strings to all lowercase to allow case sensitivty. 
+    let p = playerSelection.toLowerCase();
+    let c = computerSelection.toLowerCase();
+
+    // Create logic of who wins. 
+    // First Case: Same selection
+    if (p == c)
+        console.log(`You Tie! You both picked ${p}!`);
+    // Second Case: Not a tie and player chose rock.
+    else if (p == "rock") {
+        if (c == "paper")
+            console.log(`You Lose! ${c} beats ${p}`);
+        else 
+            console.log(`You Win! ${p} beats ${c}`);
+    }
+    // Third Case: Not a tie and player chose paper. 
+    else if (p == "paper") {
+        if (c == "scissors")
+            console.log(`You Lose! ${c} beats ${p}`);
+        else 
+            console.log(`You Win! ${p} beats ${c}`);
+    }
+    // Final Case: Not a tie and player chose scissors.
+    else {
+        if (c == "rock")
+            console.log(`You Lose! ${c} beats ${p}`);
+        else 
+            console.log(`You Win! ${p} beats ${c}`);
+    }        
+}
